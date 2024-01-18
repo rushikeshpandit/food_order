285470ac1ade51305150cba4dabbce7f24ed9726
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-18 17:24:07 +0530

    - Deleted test code

--- lib/food_order_web/live/page_live/page_live.ex
+++ lib/food_order_web/live/page_live/page_live.ex
@@ -1,16 +1,3 @@
 defmodule FoodOrderWeb.PageLive do
   use FoodOrderWeb, :live_view
-  alias FoodOrderWeb.Client, as: CL
-  alias FoodOrderWeb.PageLive.Client
-
-  def mount(_params, _session, socket) do
-    clients = CL.all()
-
-    {:ok, assign(socket, clients: clients)}
-  end
-
-  def handle_info({:change_name, id, name}, socket) do
-    send_update(Client, id: id, name: name)
-    {:noreply, socket}
-  end
 end
