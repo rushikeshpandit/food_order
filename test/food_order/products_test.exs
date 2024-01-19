31fca85988ad2f0b46d1e8eef828690c2e99d2ce
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-19 21:06:19 +0530

    - Added test

---
+++ test/food_order/products_test.exs
@@ -0,0 +1,76 @@
+defmodule FoodOrder.ProductsTest do
+  use FoodOrder.DataCase
+
+  alias FoodOrder.Products
+
+  describe "products" do
+    alias FoodOrder.Products.Product
+
+    import FoodOrder.ProductsFixtures
+
+    @invalid_attrs %{name: nil, size: nil, description: nil, price: nil}
+
+    test "list_products/0 returns all products" do
+      product = get_all_products()
+      assert Products.list_products() == product
+    end
+
+    test "get_product!/1 returns the product with given id" do
+      product = product_fixture()
+      assert Products.get_product!(product.id) == product
+    end
+
+    test "create_product/1 with valid data creates a product" do
+      valid_attrs = %{
+        name: "some name",
+        description: "some description",
+        size: "SMALL",
+        price: 42
+      }
+
+      assert {:ok, %Product{} = product} = Products.create_product(valid_attrs)
+      assert product.name == "some name"
+      assert product.description == "some description"
+      assert product.price == Money.new(42)
+      assert product.size == :SMALL
+    end
+
+    test "create_product/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Products.create_product(@invalid_attrs)
+    end
+
+    test "update_product/2 with valid data updates the product" do
+      product = product_fixture()
+
+      update_attrs = %{
+        name: "some updated name",
+        description: "some updated description",
+        price: 43,
+        size: :SMALL
+      }
+
+      assert {:ok, %Product{} = product} = Products.update_product(product, update_attrs)
+      assert product.name == "some updated name"
+      assert product.description == "some updated description"
+      assert product.price == Money.new(43)
+      assert product.size == :SMALL
+    end
+
+    test "update_product/2 with invalid data returns error changeset" do
+      product = product_fixture()
+      assert {:error, %Ecto.Changeset{}} = Products.update_product(product, @invalid_attrs)
+      assert product == Products.get_product!(product.id)
+    end
+
+    test "delete_product/1 deletes the product" do
+      product = product_fixture()
+      assert {:ok, %Product{}} = Products.delete_product(product)
+      assert_raise Ecto.NoResultsError, fn -> Products.get_product!(product.id) end
+    end
+
+    test "change_product/1 returns a product changeset" do
+      product = product_fixture()
+      assert %Ecto.Changeset{} = Products.change_product(product)
+    end
+  end
+end
