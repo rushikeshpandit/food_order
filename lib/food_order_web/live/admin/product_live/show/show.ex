8c3989e6035ac727d69457219e30d069eed64a73
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-29 21:05:53 +0530

    - Added edit functionality

--- lib/food_order_web/live/admin/product_live/show/show.ex
+++ lib/food_order_web/live/admin/product_live/show/show.ex
@@ -1,9 +1,10 @@
 defmodule FoodOrderWeb.Admin.ProductLive.Show do
   alias FoodOrder.Products
   use FoodOrderWeb, :live_view
+  alias FoodOrderWeb.Admin.ProductLive.Form
 
   def handle_params(%{"id" => id}, _uri, socket) do
     product = Products.get_product!(id)
-    {:noreply, assign(socket, :product, product)}
+    {:noreply, assign(socket, :product, product) |> assign(page_title: "Product #{product.id}")}
   end
 end
