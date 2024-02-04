5abb39342d1520c36e9a30f26c545f15be735a2a
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-04 10:40:47 +0530

    - Linting change

--- lib/food_order_web/live/admin/product_live/index/paginate/paginate.ex
+++ lib/food_order_web/live/admin/product_live/index/paginate/paginate.ex
@@ -8,7 +8,10 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index.Paginate do
         :if={@options.page > 1}
         class="h-8 w-8 mr-1 flex justify-center items-center cursor-pointer"
       >
-        <.link patch={~p"/admin/products?#{Map.update(@options, :page, @options.page, &(&1 - 1))}"} data-role="previous">
+        <.link
+          patch={~p"/admin/products?#{Map.update(@options, :page, @options.page, &(&1 - 1))}"}
+          data-role="previous"
+        >
           <.icon name="hero-backward-solid" class="h-6 w-6 text-orange-500 stroke-current" />
         </.link>
       </div>
@@ -27,7 +30,10 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index.Paginate do
         </div>
       </div>
       <div class="h-8 w-8 mr-1 flex justify-center items-center cursor-pointer">
-        <.link patch={~p"/admin/products?#{Map.update(@options, :page, @options.page, &(&1 + 1))}"} data-role="next">
+        <.link
+          patch={~p"/admin/products?#{Map.update(@options, :page, @options.page, &(&1 + 1))}"}
+          data-role="next"
+        >
           <.icon name="hero-forward-solid" class="h-6 w-6 text-orange-500 stroke-current" />
         </.link>
       </div>
