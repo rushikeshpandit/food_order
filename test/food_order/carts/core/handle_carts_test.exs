67f5bc3a45b898d713852ce8e5f0b62d7607be52
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-06 20:25:35 +0530

    - Added tests for decrement functionality

--- test/food_order/carts/core/handle_carts_test.exs
+++ test/food_order/carts/core/handle_carts_test.exs
@@ -81,4 +81,37 @@ defmodule FoodOrder.Carts.Core.HandleCartsTest do
              |> Money.add(product.price) == cart.total_price
     end
   end
+
+  describe "dec" do
+    test "dec same element" do
+      product = product_fixture()
+
+      cart =
+        @start_cart
+        |> add(product)
+        |> add(product)
+        |> dec(product.id)
+
+      assert 1 == cart.total_qty
+
+      assert product.price
+             |> Money.add(product.price)
+             |> Money.subtract(product.price) == cart.total_price
+    end
+
+    test "dec until remove the product" do
+      product = product_fixture()
+
+      cart =
+        @start_cart
+        |> add(product)
+        |> add(product)
+        |> dec(product.id)
+        |> dec(product.id)
+
+      assert 0 == cart.total_qty
+      assert [] == cart.items
+      assert Money.new(0) == cart.total_price
+    end
+  end
 end
