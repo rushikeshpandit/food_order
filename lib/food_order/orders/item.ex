6415c94210caf44ed8202286c140236647de88fe
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-09 17:36:05 +0530

    - Added item schema

---
+++ lib/food_order/orders/item.ex
@@ -0,0 +1,21 @@
+defmodule FoodOrder.Orders.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias FoodOrder.Products.Product
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "items" do
+    field :quantity, :integer
+    belongs_to :product_id, Product
+    field :order_id, :binary_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(item, attrs) do
+    item
+    |> cast(attrs, [:quantity, :product_id])
+    |> validate_required([:quantity, :product_id])
+  end
+end
