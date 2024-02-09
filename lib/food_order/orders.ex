ead152f03ae319360762ee2235093edb113694ce
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-09 21:49:34 +0530

    - Added delegate for all orders

--- lib/food_order/orders.ex
+++ lib/food_order/orders.ex
@@ -1,5 +1,8 @@
 defmodule FoodOrder.Orders do
+  alias __MODULE__.AllStatusOrders
   alias __MODULE__.Events.NewOrder
 
   defdelegate subscribe_to_receive_new_order, to: NewOrder, as: :subscribe
+
+  defdelegate all_status_orders, to: AllStatusOrders, as: :execute
 end
