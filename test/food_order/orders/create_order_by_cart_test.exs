5d36e3febd4a0867322d2bb45501406c0aa992e8
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-09 19:56:13 +0530

    - Added relevant tests

---
+++ test/food_order/orders/create_order_by_cart_test.exs
@@ -0,0 +1,31 @@
+defmodule FoodOrder.Orders.CreateOrderByCartTest do
+  use FoodOrder.DataCase
+  alias FoodOrder.Carts
+  alias FoodOrder.Orders.CreateOrderByCart
+
+  import FoodOrder.AccountsFixtures
+  import FoodOrder.ProductsFixtures
+
+  describe "create_order/1" do
+    test "create order with success" do
+      product = product_fixture()
+      user = user_fixture()
+
+      Carts.create(user.id)
+      Carts.add(user.id, product)
+
+      assert 1 == Carts.get(user.id).total_qty
+
+      payload = %{
+        "address" => "123",
+        "current_user" => user.id,
+        "phone_number" => "1234"
+      }
+
+      {:ok, result} = CreateOrderByCart.execute(payload)
+
+      assert 1 == result.total_quantity
+      assert 0 == Carts.get(user.id).total_qty
+    end
+  end
+end
