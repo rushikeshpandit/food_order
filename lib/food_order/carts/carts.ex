bf3c853849ed5830d4bb886a01d7e542243198c7
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-09 20:25:00 +0530

    - Added delete method for cart

--- lib/food_order/carts/carts.ex
+++ lib/food_order/carts/carts.ex
@@ -6,4 +6,5 @@ defmodule FoodOrder.Carts do
   def inc(cart_id, product_id), do: GenServer.call(@name, {:inc, cart_id, product_id})
   def remove(cart_id, product), do: GenServer.cast(@name, {:remove, cart_id, product})
   def dec(cart_id, product_id), do: GenServer.call(@name, {:dec, cart_id, product_id})
+  def delete(cart_id), do: GenServer.cast(@name, {:delete, cart_id})
 end
