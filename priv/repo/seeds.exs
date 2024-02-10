55521c07ce8cc766c1ee3cd2e7e10eb95b55fa9f
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-10 16:21:34 +0530

    - Updated seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,24 +1,46 @@
 alias FoodOrder.Accounts
 alias FoodOrder.Products
+alias FoodOrder.Orders.Order
+alias FoodOrder.Repo
+
+products =
+  for _ <- 0..50 do
+    {:ok, product} =
+      Products.create_product(%{
+        description: "bla",
+        name: "Pizza #{:rand.uniform(10_000)}",
+        price: :rand.uniform(10_000),
+        size: Enum.random(["SMALL", "MEDIUM", "LARGE"]),
+        image_url: "product_#{1..4 |> Enum.random()}.jpeg"
+      })
+
+    product
+  end
 
 Accounts.register_user(%{
-  email: "admin@rpandit.com",
-  password: "Admin@rp.in12",
+  email: "adm@elxpro.com",
+  password: "adm@elxpro.coM1",
   role: "ADMIN"
 })
 
-Accounts.register_user(%{
-  email: "user@rpandit.com",
-  password: "User@rp.in12",
-  role: "USER"
-})
+{:ok, user} =
+  Accounts.register_user(%{
+    email: "user@elxpro.com",
+    password: "user@elxpro.coM1",
+    role: "USER"
+  })
 
-for index <- 0..100,
-    do:
-      Products.create_product(%{
-        description: "bla bla",
-        name: "Pizza #{index}",
-        price: :rand.uniform(10_000),
-        size: Enum.random(["SMALL", "MEDIUM", "LARGE"]),
-        image_url: "product_#{1..4 |> Enum.random()}.jpg"
-      })
+[product | _rest] = products
+
+order_attrs = %{
+  phone_number: "123",
+  address: "123213",
+  user_id: user.id,
+  items: [%{quantity: 1, product_id: product.id}],
+  total_quantity: 1,
+  total_price: product.price
+}
+
+%Order{}
+|> Order.changeset(order_attrs)
+|> Repo.insert()
