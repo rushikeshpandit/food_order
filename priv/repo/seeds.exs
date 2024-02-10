68e59975623a867319e99e28d38314069c6985c8
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-10 22:21:40 +0530

    - Updated image extension in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,7 @@ products =
         name: "Pizza #{:rand.uniform(10_000)}",
         price: :rand.uniform(10_000),
         size: Enum.random(["SMALL", "MEDIUM", "LARGE"]),
-        image_url: "product_#{1..4 |> Enum.random()}.jpeg"
+        image_url: "product_#{1..4 |> Enum.random()}.jpg"
       })
 
     product
