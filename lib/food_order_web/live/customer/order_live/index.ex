63638597cf772c56095822d9126fee86d712602f
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-12 17:13:19 +0530

    - Added customer live order page

--- lib/food_order_web/live/customer/order_live/index.ex
+++ lib/food_order_web/live/customer/order_live/index.ex
@@ -1,3 +1,14 @@
 defmodule FoodOrderWeb.Customer.OrderLive.Index do
+  import Phoenix.Naming, only: [humanize: 1]
   use FoodOrderWeb, :live_view
+  alias FoodOrder.Orders
+
+  def mount(_, _, socket) do
+    current_user = socket.assigns.current_user
+    if connected?(socket), do: Orders.subscribe_update_user_row(current_user.id)
+    orders = Orders.list_orders_by_user_id(current_user.id)
+    socket = socket |> assign(orders: orders)
+
+    {:ok, socket}
+  end
 end
