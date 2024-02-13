f690231c0e99d6ced95aef1f725d69f6f7807c18
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-02-13 17:05:53 +0530

    - Updated index tests

--- test/food_order_web/live/customer/order_live/index_test.exs
+++ test/food_order_web/live/customer/order_live/index_test.exs
@@ -13,5 +13,18 @@ defmodule FoodOrderWeb.Customer.OrderLive.IndexTest do
       {:ok, view, _html} = live(conn, ~p"/customer/orders")
       assert has_element?(view, "##{order.id}")
     end
+
+    test "send to another layer using the handle_info using pid", %{conn: conn, user: user} do
+       product = product_fixture()
+       order = order_fixtures(product, user)
+
+       {:ok, view, _html} = live(conn, ~p"/customer/orders")
+       assert view |> element("td", "Not started") |> render() =~ "text-orange-600"
+
+       order = Map.put(order, :status, :DELIVERED)
+       send(view.pid, {:update_order_user_row, order})
+
+       refute view |> element("td", "Delivered") |> render() =~ "text-orange-600"
+     end
   end
 end
